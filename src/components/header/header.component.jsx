866211686457742dc9686b3c9106b958186ef456
iMadfxq <imadfxq@gmail.com>
2022-07-10 23:48:24 -0400

    Started building basic structure to begin the video

---
+++ src/components/header/header.component.jsx
@@ -0,0 +1,13 @@
+import './header.styles.css'
+
+export default function Header() {
+  return (
+    <header>
+      <h2>Your app</h2>
+      <nav>
+        <a href="/">Menu</a>
+        <a href="/">Contact</a>
+      </nav>
+    </header>
+  )
+}
